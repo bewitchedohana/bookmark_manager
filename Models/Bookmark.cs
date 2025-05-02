7b7518ebc31326922c51ad6edd8cf5ebcebdc782
BewitchedOhana <bewitchedohana@tutanota.com>
2025-05-02 19:53:06 -0300

    feat: BaseModel and Bookmark model
    Adds the BaseModel and the Bookmark model to the project, so we can use it as a communication with the Sqlite

---
+++ Models/Bookmark.cs
@@ -0,0 +1,33 @@
+﻿namespace BookmarkManager.Models;
+
+public sealed class Bookmark : BaseModel {
+	private string _url = string.Empty;
+
+	public string Url {
+		get => _url;
+		set {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+			_url = value;
+		}
+	}
+
+	private string _name = string.Empty;
+
+	public string Name {
+		get => _name;
+		set {
+			ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+			_name = value; 
+		}
+	}
+
+    public Bookmark() : base() { }
+
+    public Bookmark(string url) : this() {
+        Url = url;
+    }
+
+    public Bookmark(string url, string name) : this(url) {
+        Name = name;
+    }
+}
