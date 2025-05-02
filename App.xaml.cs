131f9f712b372e8b82359ae4d58003cecf26f0eb
BewitchedOhana <bewitchedohana@tutanota.com>
2025-05-02 19:33:24 -0300

    chore: adds dependency injection
    Adds dependency injection to the project, so it's easier to decouple the code, providing easier testing

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,25 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
-namespace BookmarkManager
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
+namespace BookmarkManager;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+    private readonly IServiceProvider _serviceProvider;
+
+    public App() {
+        IServiceCollection serviceCollection = new ServiceCollection();
+        _serviceProvider = ConfigureServices(serviceCollection).BuildServiceProvider();
     }
 
+    private IServiceCollection ConfigureServices(IServiceCollection services) {
+        services.AddSingleton<MainWindow, MainWindow>();
+        return services;
+    }
+
+    private void Application_Startup(object sender, StartupEventArgs e) {
+        _serviceProvider.GetRequiredService<MainWindow>().Show();
+    }
 }
